4c0fccfb385944f71ddde3298e659d8981fd1630
Nattawut <thisnattawut@gmail.com>
2020-12-26 23:06:45 +0700

    edit main

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ATMConfig.class);
-        ATM atm = context.getBean(ATM.class);
-        String name = atm.validateCustomer(3,7899);
-        int thb = 5000;
-        System.out.println(name + " has " + atm.getBalance() + " THB");
-        atm.withdraw(thb);
-        System.out.println(name + " withdraw " + thb + " THB");
-        System.out.println(name + " has " + atm.getBalance() + " THB left");
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+        ATM atm= context.getBean(ATM.class);
+
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
     }
 }
